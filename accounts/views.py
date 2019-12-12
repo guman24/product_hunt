98434a24d6bfc671e79062a66093d4ff22e2974e
Rajan <rajankarki0401@gmail.com>
2019-12-13 01:06:27 +0545

    make logout,login and signup completely

--- accounts/views.py
+++ accounts/views.py
@@ -23,7 +23,18 @@ def signup(request):
         return render(request,'account/sign_up.html')
 
 def login(request):
-    return render(request,'account/login.html')
+    if request.method=='POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'account/login.html',{'error':"username or password is incorrect"})
+    else:
+     return render(request,'account/login.html')
 
 def logout(request):
-    return render(request,'account/sign_up.html')
+    if request.method=='POST':
+        auth.logout(request)
+        return redirect('home')
+    
